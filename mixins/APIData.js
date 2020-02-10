be109772c0405c9c92a42e531aa813affe363b5f
Cyriac Thomas <me@cyriacthomas.com>
2020-02-10 18:28:34 +0530

    fix issue with defaults

--- mixins/APIData.js
+++ mixins/APIData.js
@@ -14,7 +14,10 @@ export default {
   },
   computed: {
     formData () {
-      let fleets = this.form.fleets.split('\n')
+      let fleets = []
+      try {
+        fleets = this.form.fleets.split('\n')
+      } catch (err) {}
       fleets = fleets.filter(f => f.length > 1)
       const fleet_timespan = this.form.fleet_timespan
       const show_names = this.form.show_names
@@ -30,11 +33,15 @@ export default {
   methods: {
     pullAPIdata () {
       const self = this
-      self.form.apikey = storage.get('apikey')
-      self.form.fleets = storage.get('fleets')
-      self.form.fleets = storage.get('fleets').join("\n")
+      self.form.apikey = storage.get('apikey') || ''
+      self.form.fleets = storage.get('fleets') || []
       self.form.show_names = storage.get('show_names')
-      self.form.fleet_timespan = storage.get('fleet_timespan')
+      self.form.fleet_timespan = storage.get('fleet_timespan') || '1440'
+
+      self.form.fleets = self.form.fleets.join("\n")
+      if (self.form.show_names === null) {
+        self.form.show_names = true
+      }
     }
   },
   mounted () {
